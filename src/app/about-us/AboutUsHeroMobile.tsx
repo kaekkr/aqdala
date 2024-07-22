7f6598abd855a32fdc642736fd50ea6a814e06ff
Karassay Raushanbek <karassayraushanbek@gmail.com>
2024-07-22 19:58:24 +0600

    add mobile about-us-hero component

--- src/app/about-us/AboutUsHeroMobile.tsx
+++ src/app/about-us/AboutUsHeroMobile.tsx
@@ -1,11 +1,11 @@
-const AboutUsHero = () => {
-    return <div className='hidden md:block bg-white text-center rounded-3xl p-5'>
+const AboutUsHeroMobile = () => {
+    return <div className='md:hidden bg-white text-center rounded-3xl p-5'>
         <p className='p3 bg-orange-100 inline rounded-2xl px-2 py-1 mx-auto'>Немножко о нас</p>
-        <h1 className='max-w-[47rem] mx-auto'>
+        <h3 className='max-w-[47rem] mx-auto'>
 				<span className='text-orange-500'>Компания Ак-Дала Агро была
 				основана в 2015 году </span>
             и находится всего
-            в 30 километрах от столицы Казахстана</h1>
+            в 30 километрах от столицы Казахстана</h3>
         <p className='p2-m text-neutral-dark max-w-2xl mx-auto'>Ак-Дала Агро – это не только высококачественная
             продукция,
             но и вклад в развитие региона и поддержку местных жителей.
@@ -25,4 +25,4 @@ const AboutUsHero = () => {
     </div>
 }
 
-export default AboutUsHero
+export default AboutUsHeroMobile
--- src/app/about-us/page.tsx
+++ src/app/about-us/page.tsx
@@ -1,13 +1,15 @@
 import AboutUsHero from '@/app/about-us/AboutUsHero'
 import Info from '@/app/about-us/Info'
 import Infrastructure from '@/app/about-us/Infrastructure'
+import AboutUsHeroMobile from "@/app/about-us/AboutUsHeroMobile";
 
 const AboutUsPage = () => {
-	return <main className='space-y-5 w-[1112px] mx-auto'>
-		<AboutUsHero />
-		<Infrastructure />
-		<Info />
-	</main>
+    return <main className='space-y-5 w-[344px] md:w-[1112px] mx-auto'>
+        <AboutUsHero/>
+        <AboutUsHeroMobile/>
+        <Infrastructure/>
+        <Info/>
+    </main>
 }
 
 export default AboutUsPage
